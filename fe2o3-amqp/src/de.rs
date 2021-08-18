1d34ee55da17ecea3e6a4b021e3583500b93a365
minghuaw <michael.wu1107@gmail.com>
2021-08-17 18:21:21 -0700

    commenting out unused category and width types

--- fe2o3-amqp/src/de.rs
+++ fe2o3-amqp/src/de.rs
@@ -1,7 +1,12 @@
 use std::{convert::TryInto};
 use serde::de;
 
-use crate::{format_code::EncodingCodes, error::Error, format::{ArrayWidth, CompoundWidth, FixedWidth, VariableWidth}, read::{IoReader, Read}};
+use crate::{
+    format_code::EncodingCodes, 
+    error::Error, 
+    // format::{ArrayWidth, CompoundWidth, FixedWidth, VariableWidth}, 
+    read::{IoReader, Read}
+};
 
 pub fn from_slice<'de, T: de::Deserialize<'de>>(slice: &'de [u8]) -> Result<T, Error> {
     let io_reader = IoReader::new(slice);
@@ -40,80 +45,80 @@ impl<'de, R: Read<'de>> Deserializer<R> {
     //     self.reader.read_bytes(n)
     // }
 
-    fn read_variable_width_bytes(&mut self, width: VariableWidth) -> Result<Vec<u8>, Error> {
-        let n = match width {
-            VariableWidth::One => {
-                let size = self.reader.next()?;
-                size as usize
-            },
-            VariableWidth::Four => {
-                let size_buf = self.reader.read_const_bytes::<4>()?;
-                u32::from_be_bytes(size_buf) as usize
-            }
-        };
+    // fn read_variable_width_bytes(&mut self, width: VariableWidth) -> Result<Vec<u8>, Error> {
+    //     let n = match width {
+    //         VariableWidth::One => {
+    //             let size = self.reader.next()?;
+    //             size as usize
+    //         },
+    //         VariableWidth::Four => {
+    //             let size_buf = self.reader.read_const_bytes::<4>()?;
+    //             u32::from_be_bytes(size_buf) as usize
+    //         }
+    //     };
 
-        self.reader.read_bytes(n)
-    }
+    //     self.reader.read_bytes(n)
+    // }
 
-    fn read_compound_bytes(&mut self, width: CompoundWidth) -> Result<CompoundBuf, Error> {
-        let cb = match width {
-            CompoundWidth::Zero => {
-                CompoundBuf { count: 0, value_buf: Vec::with_capacity(0) }
-            },
-            CompoundWidth::One => {
-                let size = self.reader.next()?;
-                let count = self.reader.next()?;
+    // fn read_compound_bytes(&mut self, width: CompoundWidth) -> Result<CompoundBuf, Error> {
+    //     let cb = match width {
+    //         CompoundWidth::Zero => {
+    //             CompoundBuf { count: 0, value_buf: Vec::with_capacity(0) }
+    //         },
+    //         CompoundWidth::One => {
+    //             let size = self.reader.next()?;
+    //             let count = self.reader.next()?;
 
-                // Need to subtract the one byte taken by `count`
-                let size = size - 1;
-                let value_buf = self.reader.read_bytes(size as usize)?;
-                CompoundBuf {count: count as u32, value_buf }
-            },
-            CompoundWidth::Four => {
-                let size_buf = self.reader.read_const_bytes::<4>()?;
-                let count_buf = self.reader.read_const_bytes::<4>()?;
-                let size = u32::from_be_bytes(size_buf);
-                let count = u32::from_be_bytes(count_buf);
+    //             // Need to subtract the one byte taken by `count`
+    //             let size = size - 1;
+    //             let value_buf = self.reader.read_bytes(size as usize)?;
+    //             CompoundBuf {count: count as u32, value_buf }
+    //         },
+    //         CompoundWidth::Four => {
+    //             let size_buf = self.reader.read_const_bytes::<4>()?;
+    //             let count_buf = self.reader.read_const_bytes::<4>()?;
+    //             let size = u32::from_be_bytes(size_buf);
+    //             let count = u32::from_be_bytes(count_buf);
 
-                // Need to substract the four byte taken by `count`
-                let size = size - 4;
-                let value_buf = self.reader.read_bytes(size as usize)?;
-                CompoundBuf { count, value_buf }
-            }
-        };
+    //             // Need to substract the four byte taken by `count`
+    //             let size = size - 4;
+    //             let value_buf = self.reader.read_bytes(size as usize)?;
+    //             CompoundBuf { count, value_buf }
+    //         }
+    //     };
 
-        Ok(cb)
-    }
+    //     Ok(cb)
+    // }
 
-    fn read_array_bytes(&mut self, width: ArrayWidth) -> Result<ArrayBuf, Error> {
-        let ab = match width {
-            ArrayWidth::One => {
-                let size = self.reader.next()?;
-                let count = self.reader.next()?;
-                let elem_code = self.reader.next()?;
+    // fn read_array_bytes(&mut self, width: ArrayWidth) -> Result<ArrayBuf, Error> {
+    //     let ab = match width {
+    //         ArrayWidth::One => {
+    //             let size = self.reader.next()?;
+    //             let count = self.reader.next()?;
+    //             let elem_code = self.reader.next()?;
 
-                // Must subtract the one byte taken by `count` and one byte taken by `elem_code`
-                let size = size - 1 - 1;
-                let value_buf = self.reader.read_bytes(size as usize)?;
-                ArrayBuf { count: count as u32, elem_code, value_buf }
-            },
-            ArrayWidth::Four => {
-                let size_buf = self.reader.read_const_bytes::<4>()?;
-                let count_buf = self.reader.read_const_bytes::<4>()?;
-                let elem_code = self.reader.next()?;
+    //             // Must subtract the one byte taken by `count` and one byte taken by `elem_code`
+    //             let size = size - 1 - 1;
+    //             let value_buf = self.reader.read_bytes(size as usize)?;
+    //             ArrayBuf { count: count as u32, elem_code, value_buf }
+    //         },
+    //         ArrayWidth::Four => {
+    //             let size_buf = self.reader.read_const_bytes::<4>()?;
+    //             let count_buf = self.reader.read_const_bytes::<4>()?;
+    //             let elem_code = self.reader.next()?;
 
-                let size = u32::from_be_bytes(size_buf);
-                let count = u32::from_be_bytes(count_buf);
+    //             let size = u32::from_be_bytes(size_buf);
+    //             let count = u32::from_be_bytes(count_buf);
 
-                // Must subtract the four bytes taken by `count` and one byte taken by `elem_code`
-                let size = size - 4 - 1;
-                let value_buf = self.reader.read_bytes(size as usize)?;
-                ArrayBuf { count, elem_code, value_buf }
-            }
-        };
+    //             // Must subtract the four bytes taken by `count` and one byte taken by `elem_code`
+    //             let size = size - 4 - 1;
+    //             let value_buf = self.reader.read_bytes(size as usize)?;
+    //             ArrayBuf { count, elem_code, value_buf }
+    //         }
+    //     };
 
-        Ok(ab)
-    }
+    //     Ok(ab)
+    // }
 
     #[inline]
     fn parse_bool(&mut self) -> Result<bool, Error> {
--- fe2o3-amqp/src/format.rs
+++ fe2o3-amqp/src/format.rs
@@ -1,122 +1,137 @@
-use std::convert::TryFrom;
+// use std::convert::TryFrom;
 
-use crate::{format_code::EncodingCodes, error::Error};
+// use crate::{format_code::EncodingCodes, error::Error};
 
-pub enum Category {
-    Fixed(FixedWidth),
-    Variable(VariableWidth),
-    Compound(CompoundWidth),
-    Array(ArrayWidth),
-}
+// offset includes 1 byte of `count`
+pub const OFFSET_LIST8: usize = 1;
+// offset includes 4 byte of `count`
+pub const OFFSET_LIST32: usize = 4;
 
-#[repr(u8)]
-pub enum FixedWidth {
-    Zero = 0,
-    One = 1,
-    Two = 2,
-    Four = 4,
-    Eight = 8,
-    Sixteen = 16,
-}
+// offset includes 1 byte of `count`
+pub const OFFSET_MAP8: usize = 1;
+// offset includes 4 byte of `count`
+pub const OFFSET_MAP32: usize = 4;
 
-#[repr(u8)]
-pub enum VariableWidth {
-    One = 1,
-    Four = 4,
-}
+// offset includes 1 byte of `count` and 1 byte of element format code
+pub const OFFSET_ARRAY8: usize = 2;
+// offset includes 4 bytes of `count` and 1 byte of element format code
+pub const OFFSET_ARRAY32: usize = 5;
 
-#[repr(u8)]
-pub enum CompoundWidth {
-    Zero = 0,
-    One = 1,
-    Four = 4,
-}
+// pub enum Category {
+//     Fixed(FixedWidth),
+//     Variable(VariableWidth),
+//     Compound(CompoundWidth),
+//     Array(ArrayWidth),
+// }
 
-#[repr(u8)]
-pub enum ArrayWidth {
-    One = 1,
-    Four = 4,
-}
+// #[repr(u8)]
+// pub enum FixedWidth {
+//     Zero = 0,
+//     One = 1,
+//     Two = 2,
+//     Four = 4,
+//     Eight = 8,
+//     Sixteen = 16,
+// }
 
-impl TryFrom<EncodingCodes> for Category {
-    type Error = Error;
+// #[repr(u8)]
+// pub enum VariableWidth {
+//     One = 1,
+//     Four = 4,
+// }
 
-    fn try_from(value: EncodingCodes) -> Result<Self, Self::Error> {
-        let value = match value {
-            EncodingCodes::DescribedType => return Err(Error::IsDescribedType),
+// #[repr(u8)]
+// pub enum CompoundWidth {
+//     Zero = 0,
+//     One = 1,
+//     Four = 4,
+// }
 
-            EncodingCodes::Null => Category::Fixed(FixedWidth::Zero),
+// #[repr(u8)]
+// pub enum ArrayWidth {
+//     One = 1,
+//     Four = 4,
+// }
 
-            EncodingCodes::Boolean => Category::Fixed(FixedWidth::One),
-            EncodingCodes::BooleanTrue => Category::Fixed(FixedWidth::Zero),
-            EncodingCodes::BooleanFalse => Category::Fixed(FixedWidth::Zero),
+// impl TryFrom<EncodingCodes> for Category {
+//     type Error = Error;
 
-            // u8
-            EncodingCodes::Ubyte => Category::Fixed(FixedWidth::One),
+//     fn try_from(value: EncodingCodes) -> Result<Self, Self::Error> {
+//         let value = match value {
+//             EncodingCodes::DescribedType => return Err(Error::IsDescribedType),
 
-            // u16
-            EncodingCodes::Ushort => Category::Fixed(FixedWidth::Two),
+//             EncodingCodes::Null => Category::Fixed(FixedWidth::Zero),
 
-            // u32
-            EncodingCodes::Uint => Category::Fixed(FixedWidth::Four),
-            EncodingCodes::SmallUint => Category::Fixed(FixedWidth::One),
-            EncodingCodes::Uint0 => Category::Fixed(FixedWidth::Zero),
+//             EncodingCodes::Boolean => Category::Fixed(FixedWidth::One),
+//             EncodingCodes::BooleanTrue => Category::Fixed(FixedWidth::Zero),
+//             EncodingCodes::BooleanFalse => Category::Fixed(FixedWidth::Zero),
 
-            // u64
-            EncodingCodes::Ulong => Category::Fixed(FixedWidth::Eight),
-            EncodingCodes::SmallUlong => Category::Fixed(FixedWidth::One),
-            EncodingCodes::Ulong0 => Category::Fixed(FixedWidth::Zero),
+//             // u8
+//             EncodingCodes::Ubyte => Category::Fixed(FixedWidth::One),
 
-            // i8
-            EncodingCodes::Byte => Category::Fixed(FixedWidth::One),
+//             // u16
+//             EncodingCodes::Ushort => Category::Fixed(FixedWidth::Two),
 
-            // i16
-            EncodingCodes::Short => Category::Fixed(FixedWidth::Two),
+//             // u32
+//             EncodingCodes::Uint => Category::Fixed(FixedWidth::Four),
+//             EncodingCodes::SmallUint => Category::Fixed(FixedWidth::One),
+//             EncodingCodes::Uint0 => Category::Fixed(FixedWidth::Zero),
 
-            // i32
-            EncodingCodes::Int => Category::Fixed(FixedWidth::Four),
-            EncodingCodes::SmallInt => Category::Fixed(FixedWidth::One),
+//             // u64
+//             EncodingCodes::Ulong => Category::Fixed(FixedWidth::Eight),
+//             EncodingCodes::SmallUlong => Category::Fixed(FixedWidth::One),
+//             EncodingCodes::Ulong0 => Category::Fixed(FixedWidth::Zero),
 
-            // i64
-            EncodingCodes::Long => Category::Fixed(FixedWidth::Eight),
-            EncodingCodes::SmallLong => Category::Fixed(FixedWidth::One),
+//             // i8
+//             EncodingCodes::Byte => Category::Fixed(FixedWidth::One),
 
-            // f32
-            EncodingCodes::Float => Category::Fixed(FixedWidth::Four),
+//             // i16
+//             EncodingCodes::Short => Category::Fixed(FixedWidth::Two),
 
-            // f64
-            EncodingCodes::Double => Category::Fixed(FixedWidth::Eight),
+//             // i32
+//             EncodingCodes::Int => Category::Fixed(FixedWidth::Four),
+//             EncodingCodes::SmallInt => Category::Fixed(FixedWidth::One),
 
-            EncodingCodes::Decimal32 => Category::Fixed(FixedWidth::Four),
-            EncodingCodes::Decimal64 => Category::Fixed(FixedWidth::Eight),
-            EncodingCodes::Decimal128 => Category::Fixed(FixedWidth::Sixteen),
+//             // i64
+//             EncodingCodes::Long => Category::Fixed(FixedWidth::Eight),
+//             EncodingCodes::SmallLong => Category::Fixed(FixedWidth::One),
 
-            EncodingCodes::Char => Category::Fixed(FixedWidth::Four),
+//             // f32
+//             EncodingCodes::Float => Category::Fixed(FixedWidth::Four),
 
-            EncodingCodes::Timestamp => Category::Fixed(FixedWidth::Eight),
+//             // f64
+//             EncodingCodes::Double => Category::Fixed(FixedWidth::Eight),
 
-            EncodingCodes::Uuid => Category::Fixed(FixedWidth::Sixteen),
+//             EncodingCodes::Decimal32 => Category::Fixed(FixedWidth::Four),
+//             EncodingCodes::Decimal64 => Category::Fixed(FixedWidth::Eight),
+//             EncodingCodes::Decimal128 => Category::Fixed(FixedWidth::Sixteen),
 
-            EncodingCodes::VBin8 => Category::Variable(VariableWidth::One),
-            EncodingCodes::VBin32 => Category::Variable(VariableWidth::Four),
+//             EncodingCodes::Char => Category::Fixed(FixedWidth::Four),
 
-            EncodingCodes::Str8 => Category::Variable(VariableWidth::One),
-            EncodingCodes::Str32 => Category::Variable(VariableWidth::Four),
+//             EncodingCodes::Timestamp => Category::Fixed(FixedWidth::Eight),
 
-            EncodingCodes::Sym8 => Category::Variable(VariableWidth::One),
-            EncodingCodes::Sym32 => Category::Variable(VariableWidth::Four),
+//             EncodingCodes::Uuid => Category::Fixed(FixedWidth::Sixteen),
 
-            EncodingCodes::List0 => Category::Compound(CompoundWidth::Zero),
-            EncodingCodes::List8 => Category::Compound(CompoundWidth::One),
-            EncodingCodes::List32 => Category::Compound(CompoundWidth::Four),
+//             EncodingCodes::VBin8 => Category::Variable(VariableWidth::One),
+//             EncodingCodes::VBin32 => Category::Variable(VariableWidth::Four),
 
-            EncodingCodes::Map8 => Category::Compound(CompoundWidth::One),
-            EncodingCodes::Map32 => Category::Compound(CompoundWidth::Four),
+//             EncodingCodes::Str8 => Category::Variable(VariableWidth::One),
+//             EncodingCodes::Str32 => Category::Variable(VariableWidth::Four),
 
-            EncodingCodes::Array8 => Category::Array(ArrayWidth::One),
-            EncodingCodes::Array32 => Category::Array(ArrayWidth::Four),
-        };
+//             EncodingCodes::Sym8 => Category::Variable(VariableWidth::One),
+//             EncodingCodes::Sym32 => Category::Variable(VariableWidth::Four),
 
-        Ok(value)
-    }
-}
+//             EncodingCodes::List0 => Category::Compound(CompoundWidth::Zero),
+//             EncodingCodes::List8 => Category::Compound(CompoundWidth::One),
+//             EncodingCodes::List32 => Category::Compound(CompoundWidth::Four),
+
+//             EncodingCodes::Map8 => Category::Compound(CompoundWidth::One),
+//             EncodingCodes::Map32 => Category::Compound(CompoundWidth::Four),
+
+//             EncodingCodes::Array8 => Category::Array(ArrayWidth::One),
+//             EncodingCodes::Array32 => Category::Array(ArrayWidth::Four),
+//         };
+
+//         Ok(value)
+//     }
+// }
