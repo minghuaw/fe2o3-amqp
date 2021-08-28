b63185c66af193890997fc0741486e294edff400
minghuaw <michael.wu1107@gmail.com>
2021-08-27 17:17:30 -0700

    added disposition

--- fe2o3-transport/src/performatives/disposition.rs
+++ fe2o3-transport/src/performatives/disposition.rs
@@ -1,1 +1,16 @@
+use serde::{Deserialize, Serialize};
+use fe2o3_amqp::{macros::Described, types::Boolean};
 
+use crate::{DeliveryState, definitions::{DeliveryNumber, Role}};
+
+#[derive(Debug, Deserialize, Serialize, Described)]
+#[serde(rename_all = "kebab-case")]
+#[amqp_contract(name="amqp:disposition:list", code=0x0000_0000_0000_0015, encoding="list")]
+pub struct Disposition {
+    role: Role,
+    first: DeliveryNumber,
+    last: Option<DeliveryNumber>,
+    settled: Option<Boolean>,
+    state: Option<DeliveryState>,
+    batchable: Option<Boolean>
+}
