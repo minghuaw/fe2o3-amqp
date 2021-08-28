7865683de81da5d96dee8b9e7db2ba51a5282466
minghuaw <michael.wu1107@gmail.com>
2021-08-27 17:21:31 -0700

    added Detach

--- fe2o3-transport/src/performatives/detach.rs
+++ fe2o3-transport/src/performatives/detach.rs
@@ -1,1 +1,14 @@
+use serde::{Deserialize, Serialize};
 
+use fe2o3_amqp::{macros::Described, types::Boolean};
+
+use crate::definitions::{Error, Handle};
+
+#[derive(Debug, Deserialize, Serialize, Described)]
+#[serde(rename_all = "kebab-case")]
+#[amqp_contract(name="amqp:detach:list", code=0x0000_0000_0000_0016, encoding="list")]
+pub struct Detach {
+    handle: Handle,
+    closed: Option<Boolean>,
+    error: Option<Error>
+}
