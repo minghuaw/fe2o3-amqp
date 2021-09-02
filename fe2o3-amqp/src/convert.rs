583bcc2d9f20fd2f7af029fd852c33308b6c8a64
minghuaw <michael.wu1107@gmail.com>
2021-09-02 15:00:55 -0700

    removed unused convert mod

--- fe2o3-amqp/src/convert.rs
+++
@@ -1,9 +0,0 @@
-// use crate::types::Described;
-
-// pub trait IntoDescribed: Sized {
-//     fn into_described(self) -> Described<Self>;
-// }
-
-// pub trait AsDescribed {
-//     fn as_described(&self) -> Described<&Self>;
-// }
