697af9a4afd5d3ba324db9800d0591577d766fd3
minghuaw <michael.wu1107@gmail.com>
2021-09-02 12:33:47 -0700

    added MaxFrameSize and ChannelMax wrappers

--- fe2o3-core/src/performatives/flow.rs
+++ fe2o3-core/src/performatives/flow.rs
@@ -5,6 +5,12 @@ use fe2o3_amqp::{
 
 use crate::definitions::{Fields, Handle, SequenceNo, TransferNumber};
 
+
+/// 2.7.4 Flow
+/// Update link state.
+/// <type name="flow" class="composite" source="list" provides="frame">
+///     <descriptor name="amqp:flow:list" code="0x00000000:0x00000013"/>
+/// </type>
 #[derive(Debug, DeserializeComposite, SerializeComposite)]
 // #[serde(rename_all = "kebab-case")]
 #[amqp_contract(
@@ -14,17 +20,38 @@ use crate::definitions::{Fields, Handle, SequenceNo, TransferNumber};
     rename_all = "kebab-case"
 )]
 pub struct Flow {
+    /// <field name="next-incoming-id" type="transfer-number"/>
     pub next_incoming_id: Option<TransferNumber>,
+    
+    /// <field name="incoming-window" type="uint" mandatory="true"/>
     pub incoming_window: Uint,
+    
+    /// <field name="next-outgoing-id" type="transfer-number" mandatory="true"/>
     pub next_outgoing_id: TransferNumber,
+    
+    /// <field name="outgoing-window" type="uint" mandatory="true"/>
     pub outgoing_window: Uint,
+    
+    /// <field name="handle" type="handle"/>
     pub handle: Option<Handle>,
+    
+    /// <field name="delivery-count" type="sequence-no"/>
     pub delivery_count: Option<SequenceNo>,
+    
+    /// <field name="link-credit" type="uint"/>
     pub link_credit: Option<Uint>,
+    
+    ///     <field name="available" type="uint"/>
     pub available: Option<Uint>,
+    
+    /// <field name="drain" type="boolean" default="false"/>
     #[amqp_contract(default)]
     pub drain: Boolean,
+    
+    /// <field name="echo" type="boolean" default="false"/>
     #[amqp_contract(default)]
     pub echo: Boolean,
+    
+    /// <field name="properties" type="fields"/>
     pub properties: Option<Fields>,
 }
--- fe2o3-core/src/performatives/open.rs
+++ fe2o3-core/src/performatives/open.rs
@@ -2,9 +2,15 @@ use fe2o3_amqp::{
     macros::{DeserializeComposite, SerializeComposite},
     types::{Symbol, Uint, Ushort},
 };
+use serde::{Serialize, Deserialize};
 
 use crate::definitions::{Fields, IetfLanguageTag, Milliseconds};
 
+
+/// Negotiate connection parameters.
+/// <type name="open" class="composite" source="list" provides="frame">
+///     <descriptor name="amqp:open:list" code="0x00000000:0x00000010"/>
+/// </type>
 #[derive(Debug, DeserializeComposite, SerializeComposite)]
 // #[serde(rename_all = "kebab-case")]
 #[amqp_contract(
@@ -14,16 +20,78 @@ use crate::definitions::{Fields, IetfLanguageTag, Milliseconds};
     rename_all = "kebab-case"
 )]
 pub struct Open {
+    /// <field name="container-id" type="string" mandatory="true"/>
     pub container_id: String,
+    
+    /// <field name="hostname" type="string"/>
     pub hostname: Option<String>,
+    
+    /// <field name="max-frame-size" type="uint" default="4294967295"/>
     #[amqp_contract(default)]
     pub max_frame_size: MaxFrameSize,
+    
+    /// <field name="channel-max" type="ushort" default="65535"/>
     #[amqp_contract(default)]
     pub channel_max: ChannelMax,
+    
+    /// <field name="idle-time-out" type="milliseconds"/>
     pub idle_time_out: Option<Milliseconds>,
+    
+    /// <field name="outgoing-locales" type="ietf-language-tag" multiple="true"/>
     pub outgoing_locales: Option<Vec<IetfLanguageTag>>,
+    
+    /// <field name="incoming-locales" type="ietf-language-tag" multiple="true"/>
     pub incoming_locales: Option<Vec<IetfLanguageTag>>,
+    
+    /// <field name="offered-capabilities" type="symbol" multiple="true"/>
     pub offered_capabilities: Option<Vec<Symbol>>,
+    
+    /// <field name="desired-capabilities" type="symbol" multiple="true"/>
     pub desired_capabilities: Option<Vec<Symbol>>,
+    
+    /// <field name="properties" type="fields"/>
     pub properties: Option<Fields>,
+}
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+pub struct MaxFrameSize(pub Uint);
+
+impl Default for MaxFrameSize {
+    fn default() -> Self {
+        MaxFrameSize(u32::MAX)
+    }
+}
+
+impl From<Uint> for MaxFrameSize {
+    fn from(value: Uint) -> Self {
+        Self(value)
+    }
+}
+
+impl From<MaxFrameSize> for Uint {
+    fn from(value: MaxFrameSize) -> Self {
+        value.0
+    }
+}
+
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+pub struct ChannelMax(pub Ushort);
+
+impl Default for ChannelMax {
+    fn default() -> Self {
+        Self(u16::MAX)
+    }
+}
+
+impl From<Ushort> for ChannelMax {
+    fn from(value: Ushort) -> Self {
+        Self(value)
+    }
+}
+
+impl From<ChannelMax> for Ushort {
+    fn from(value: ChannelMax) -> Self {
+        value.0
+    }
 }
