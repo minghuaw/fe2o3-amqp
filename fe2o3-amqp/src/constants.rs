5dac6a2f7faa2ec15d0542c21504549e11e71af7
minghuaw <michael.wu1107@gmail.com>
2021-10-02 00:16:14 -0700

    changed constants used in serde

--- fe2o3-amqp/src/constants.rs
+++ fe2o3-amqp/src/constants.rs
@@ -1,14 +1,16 @@
-pub const DESCRIBED_BASIC: &str = "DESCRIBED_BASIC";
-pub const DESCRIBED_LIST: &str = "DESCRIBED_LIST";
-pub const DESCRIBED_MAP: &str = "DESCRIBED_MAP";
+// Public constants that are also used in derive macros
+pub const DESCRIBED_BASIC: &str = "AMQP1.0_DESCRIBED_BASIC";
+pub const DESCRIBED_LIST: &str = "AMQP1.0_DESCRIBED_LIST";
+pub const DESCRIBED_MAP: &str = "AMQP1.0_DESCRIBED_MAP";
+pub const DESCRIPTOR: &str = "AMQP1.0_DESCRIPTOR";
 
-pub const DESCRIPTOR: &str = "DESCRIPTOR";
-pub(crate) const VALUE: &str = "VALUE";
-
-pub(crate) const ARRAY: &str = "ARRAY";
-pub(crate) const DECIMAL32: &str = "DECIMAL32";
-pub(crate) const DECIMAL64: &str = "DECIMAL64";
-pub(crate) const DECIMAL128: &str = "DECIMAL128";
-pub(crate) const SYMBOL: &str = "SYMBOL";
-pub(crate) const TIMESTAMP: &str = "TIMESTAMP";
-pub(crate) const UUID: &str = "UUID";
+// These constants should only be public on the crate level
+// to avoid accidental misuse
+pub(crate) const VALUE: &str = "AMQP1.0_VALUE";
+pub(crate) const ARRAY: &str = "AMQP1.0_ARRAY";
+pub(crate) const DECIMAL32: &str = "AMQP1.0_DECIMAL32";
+pub(crate) const DECIMAL64: &str = "AMQP1.0_DECIMAL64";
+pub(crate) const DECIMAL128: &str = "AMQP1.0_DECIMAL128";
+pub(crate) const SYMBOL: &str = "AMQP1.0_SYMBOL";
+pub(crate) const TIMESTAMP: &str = "AMQP1.0_TIMESTAMP";
+pub(crate) const UUID: &str = "AMQP1.0_UUID";
