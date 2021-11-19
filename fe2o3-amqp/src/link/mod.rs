8c0d44112e16cfac33d9486e44573e4880035218
minghuaw <michael.wu1107@gmail.com>
2021-11-19 10:39:49 -0800

    changed LinkFlowState to use atomics

--- fe2o3-amqp/src/link/mod.rs
+++ fe2o3-amqp/src/link/mod.rs
@@ -1,4 +1,6 @@
 mod frame;
+use std::sync::{Arc, atomic::{AtomicBool, AtomicU32}};
+
 use fe2o3_amqp_types::{definitions::{Fields, SequenceNo}, performatives::Flow};
 pub use frame::*;
 pub mod builder;
@@ -9,6 +11,7 @@ pub mod sender_link;
 
 pub use receiver::Receiver;
 pub use sender::Sender;
+use tokio::sync::{RwLock, mpsc};
 
 use crate::error::EngineError;
 
@@ -45,15 +48,20 @@ pub enum LinkState {
 }
 
 pub(crate) struct LinkFlowState {
-    delivery_count: SequenceNo,
-    link_credit: u32,
-    avaiable: u32,
-    drain: bool,
-    properties: Option<Fields>,
+    delivery_count: AtomicU32, // SequenceNo = u32
+    link_credit: AtomicU32,
+    avaiable: AtomicU32,
+    drain: AtomicBool,
+    properties: RwLock<Option<Fields>>,
 }
 
 impl LinkFlowState {
     pub(crate) fn on_incoming_flow(&mut self, flow: &Flow) -> Result<Option<Flow>, EngineError> {
         todo!()
     }
+}
+
+pub(crate) struct LinkHandle {
+    tx: mpsc::Sender<LinkIncomingItem>,
+    state: Arc<LinkFlowState>,
 }
