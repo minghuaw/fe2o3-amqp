7a04aeaa8fa48469054ecf0c0cf625705f2a6397
minghuaw <michael.wu1107@gmail.com>
2022-04-17 04:08:30 -0700

    modified listener test example

--- examples/protocol_test/src/bin/listener.rs
+++ examples/protocol_test/src/bin/listener.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 use fe2o3_amqp::{listener::{ConnectionAcceptor, session::SessionAcceptor}};
 use tokio::net::TcpListener;
 use tracing::{Level, instrument};
@@ -10,17 +12,24 @@ async fn connection_main() {
     let tcp_listener = TcpListener::bind(BASE_ADDR).await.unwrap();
     let connection_acceptor = ConnectionAcceptor::new("test_conn_listener");
     let session_acceptor = SessionAcceptor::default();
+    let mut sessions = Vec::new();
 
     loop {
         let (stream, addr) = tcp_listener.accept().await.unwrap();
         println!("Incoming connection from {:?}", addr);
         let mut connection = connection_acceptor.accept(stream).await.unwrap();
-        let mut session = session_acceptor.accept(&mut connection).await.unwrap();
-        
 
+        for _ in 0..2 {
+            if let Ok(session) = session_acceptor.accept(&mut connection).await {
+                sessions.push(session);
+            }
+        }
         
-        let result = session.close().await;
-        println!("Session close result: {:?}", result);
+        for mut session in sessions.drain(..) {
+            let result = session.close().await;
+            println!("Session close result: {:?}", result);
+        }
+
         let result = connection.close().await;
         println!("Connection close result: {:?}", result);
     }
