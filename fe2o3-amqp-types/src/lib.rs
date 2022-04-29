93e8b3903490d95e5f3918591865761685f19caf
minghuaw <michael.wu1107@gmail.com>
2022-04-28 18:49:09 -0700

    tested feature flags deps

--- fe2o3-amqp-types/src/lib.rs
+++ fe2o3-amqp-types/src/lib.rs
@@ -2,10 +2,23 @@
 
 //! Implements AMQP1.0 data types as defined in the [specification](http://docs.oasis-open.org/amqp/core/v1.0/os/amqp-core-overview-v1.0-os.html).
 
-pub mod definitions;
-pub mod messaging;
-pub mod performatives;
+#[cfg(feature = "primitive")]
 pub mod primitives;
+
+#[cfg(feature = "transport")]
+pub mod definitions;
+
+#[cfg(feature = "messaging")]
+pub mod messaging;
+
+#[cfg(all(feature = "transport", feature = "messaging"))]
+pub mod performatives;
+
+#[cfg(feature = "security")]
 pub mod sasl;
+
+#[cfg(feature = "transport")]
 pub mod states;
+
+#[cfg(feature = "transaction")]
 pub mod transaction;
