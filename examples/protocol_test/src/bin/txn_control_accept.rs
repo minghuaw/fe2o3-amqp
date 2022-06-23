e70f29bfc9f156ca6fc4308d6cd6e909cbb00f39
minghuaw <michael.wu1107@gmail.com>
2022-06-23 06:21:43 -0700

    cargo fmt

--- examples/protocol_test/src/bin/txn_control_accept.rs
+++ examples/protocol_test/src/bin/txn_control_accept.rs
@@ -1,8 +1,12 @@
-use fe2o3_amqp::acceptor::{ConnectionAcceptor, SaslPlainMechanism};
+use fe2o3_amqp::{
+    acceptor::{
+        ConnectionAcceptor, LinkAcceptor, LinkEndpoint, SaslPlainMechanism, SessionAcceptor,
+    },
+    types::primitives::Value,
+};
 use tokio::net::TcpListener;
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
-
 
 const BASE_ADDR: &str = "localhost:5672";
 
@@ -15,15 +19,53 @@ async fn main() {
 
     let tcp_listener = TcpListener::bind(BASE_ADDR).await.unwrap();
 
-    // let connection_acceptor = ConnectionAcceptor::new("test_conn_listener");
-    let connection_acceptor = ConnectionAcceptor::builder()
-        .container_id("example_connection_acceptor")
-        .sasl_acceptor(SaslPlainMechanism::new("guest", "guest"))
-        .build();
+    let connection_acceptor = ConnectionAcceptor::new("test_conn_listener");
+    // let connection_acceptor = ConnectionAcceptor::builder()
+    //     .container_id("example_connection_acceptor")
+    //     .sasl_acceptor(SaslPlainMechanism::new("guest", "guest"))
+    //     .build();
 
     while let Ok((stream, addr)) = tcp_listener.accept().await {
-        
-    }
+        tracing::info!("Incoming connection from {:?}", addr);
+        let mut connection = connection_acceptor.accept(stream).await.unwrap();
 
-    todo!()
+        let _ = tokio::spawn(async move {
+            let session_acceptor = SessionAcceptor::default();
+
+            while let Ok(mut session) = session_acceptor.accept(&mut connection).await {
+                let _ = tokio::spawn(async move {
+                    tracing::info!("Incoming session created");
+
+                    let link_acceptor = LinkAcceptor::new();
+
+                    while let Ok(link) = link_acceptor.accept(&mut session).await {
+                        match link {
+                            LinkEndpoint::Sender(mut sender) => {
+                                let _ = tokio::spawn(async move {
+                                    tracing::info!("Incoming link is connected (remote: receiver, local: sender)");
+                                    sender.send("world").await.unwrap();
+                                    if let Err(e) = sender.close().await {
+                                        // The remote may close the session
+                                        tracing::error!(link="sender", error=?e);
+                                    }
+                                });
+                            }
+                            LinkEndpoint::Receiver(mut recver) => {
+                                let _ = tokio::spawn(async move {
+                                    tracing::info!("Incoming link is connected (remote: sender, local: receiver");
+                                    let delivery = recver.recv::<Value>().await.unwrap();
+                                    tracing::info!(message = ?delivery.message());
+                                    recver.accept(&delivery).await.unwrap();
+                                    if let Err(e) = recver.close().await {
+                                        // The remote may close the session
+                                        tracing::error!(link="receiver", error=?e);
+                                    }
+                                });
+                            }
+                        }
+                    }
+                });
+            }
+        });
+    }
 }
--- examples/protocol_test/src/bin/txn_control_init.rs
+++ examples/protocol_test/src/bin/txn_control_init.rs
@@ -1,3 +1,50 @@
-fn main() {
-    todo!()
+use std::time::Duration;
+
+use fe2o3_amqp::{types::primitives::Value, Connection, Delivery, Receiver, Sender, Session};
+use tracing::{instrument, Level};
+use tracing_subscriber::FmtSubscriber;
+
+const SASL_PLAIN: &str = "";
+// const SASL_PLAIN: &str = "guest:guest";
+const BASE_ADDR: &str = "localhost:5672";
+
+#[instrument]
+async fn client_main() {
+    let url = format!("amqp://{}@{}", SASL_PLAIN, BASE_ADDR);
+
+    let mut connection = Connection::open("connection-1", &url[..]).await.unwrap();
+    let mut session = Session::begin(&mut connection).await.unwrap();
+
+    // Test a regular receiver
+    let mut receiver = Receiver::attach(&mut session, "receiver-1", "q1")
+        .await
+        .unwrap();
+    let delivery: Delivery<Value> = receiver.recv().await.unwrap();
+    tracing::info!(message = ?delivery.message());
+    receiver.accept(&delivery).await.unwrap();
+    
+    // Test a regular sender
+    let mut sender = Sender::attach(&mut session, "sender-1", "q1")
+        .await
+        .unwrap();
+    sender.send("hello").await.unwrap();
+
+    // Test creating a control link
+    let controller = Controller::attach(&mut session, "controller", Coordinator::default())
+        .await?;
+    
+    receiver.close().await.unwrap();
+    sender.close().await.unwrap();
+    session.end().await.unwrap();
+    connection.close().await.unwrap();
+}
+
+#[tokio::main]
+async fn main() {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    client_main().await;
 }
