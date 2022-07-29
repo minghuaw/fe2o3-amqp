b45117c0a0825df50b9d99835eb56cf6c1070cb1
minghuaw <michael.wu1107@gmail.com>
2022-07-28 23:04:38 -0700

    lay out traits that need implementing

--- fe2o3-amqp-ws/src/lib.rs
+++ fe2o3-amqp-ws/src/lib.rs
@@ -1,3 +1,59 @@
+use futures_util::{Sink, Stream};
+use tokio::io::{AsyncRead, AsyncWrite};
+use tungstenite::Message;
+
+pub struct WebSocketStream<S> {
+    inner: S,
+}
+
+impl<S> From<tokio_tungstenite::WebSocketStream<S>>
+    for WebSocketStream<tokio_tungstenite::WebSocketStream<S>>
+{
+    fn from(inner: tokio_tungstenite::WebSocketStream<S>) -> Self {
+        Self { inner }
+    }
+}
+
+impl<S> AsyncRead for WebSocketStream<S>
+where
+    S: Sink<Message>,
+{
+    fn poll_read(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &mut tokio::io::ReadBuf<'_>,
+    ) -> std::task::Poll<std::io::Result<()>> {
+        todo!()
+    }
+}
+
+impl<S> AsyncWrite for WebSocketStream<S>
+where
+    S: Stream<Item = Message>,
+{
+    fn poll_write(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &[u8],
+    ) -> std::task::Poll<Result<usize, std::io::Error>> {
+        todo!()
+    }
+
+    fn poll_flush(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), std::io::Error>> {
+        todo!()
+    }
+
+    fn poll_shutdown(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), std::io::Error>> {
+        todo!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
