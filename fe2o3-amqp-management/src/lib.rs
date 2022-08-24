54bc4f8afd6e13d0e2f0421c6d48958e04aeaee3
minghuaw <michael.wu1107@gmail.com>
2022-08-24 07:01:30 -0700

    initial commit for amqp management

---
+++ fe2o3-amqp-management/src/lib.rs
@@ -0,0 +1,11 @@
+
+pub const MANAGEMENT_NODE_ADDRESS: &str = "$management";
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
