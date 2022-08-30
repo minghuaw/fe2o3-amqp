21e092de4847138e2be58311b42315f29ace83c2
minghuaw <michael.wu1107@gmail.com>
2022-08-30 14:55:45 -0700

    added docs

--- fe2o3-amqp-types/src/messaging/format/mod.rs
+++ fe2o3-amqp-types/src/messaging/format/mod.rs
@@ -68,10 +68,25 @@ impl From<Priority> for UByte {
     }
 }
 
-/// 3.2.2 Delivery Annotations
-/// <type name="delivery-annotations" class="restricted" source="annotations" provides="section">
-///     <descriptor name="amqp:delivery-annotations:map" code="0x00000000:0x00000071"/>
+/// 3.2.2 Delivery Annotations 
+/// 
+/// <type name="delivery-annotations" class="restricted" source="annotations" provides="section"> 
+///     <descriptor name="amqp:delivery-annotations:map" code="0x00000000:0x00000071"/> 
 /// </type>
+/// 
+/// The delivery-annotations section is used for delivery-specific non-standard properties at the
+/// head of the message. Delivery annotations convey information from the sending peer to the
+/// receiving peer. If the recipient does not understand the annotation it cannot be acted upon and
+/// its effects (such as any implied propagation) cannot be acted upon. Annotations might be
+/// specific to one implementation, or common to multiple implementations. The capabilities
+/// negotiated on link attach and on the source and target SHOULD be used to establish which anno-
+/// tations a peer supports. A registry of defined annotations and their meanings is maintained
+/// [AMQPDELANN]. The symbolic key “rejected” is reserved for the use of communicating error
+/// information regarding rejected messages. Any values associated with the “rejected” key MUST be
+/// of type error.
+/// 
+/// If the delivery-annotations section is omitted, it is equivalent to a delivery-annotations
+/// section containing an empty map of annotations.
 #[derive(Debug, Clone, Default, DeserializeComposite, SerializeComposite)]
 #[amqp_contract(
     name = "amqp:delivery-annotations:map",
@@ -101,10 +116,25 @@ impl DerefMut for DeliveryAnnotations {
     }
 }
 
-/// 3.2.3 Message Annotations
-/// <type name="message-annotations" class="restricted" source="annotations" provides="section">
-///     <descriptor name="amqp:message-annotations:map" code="0x00000000:0x00000072"/>
-/// </type>
+/// 3.2.3 Message Annotations <type name="message-annotations" class="restricted"
+/// source="annotations" provides="section"> <descriptor name="amqp:message-annotations:map"
+///     code="0x00000000:0x00000072"/> </type>
+/// 
+/// The message-annotations section is used for properties of the message which are aimed at the
+/// infrastructure and SHOULD be propagated across every delivery step. Message annotations convey
+/// information about the message. Intermediaries MUST propagate the annotations unless the
+/// annotations are explicitly augmented or modified (e.g., by the use of the modified outcome).
+///
+/// The capabilities negotiated on link attach and on the source and target can be used to establish
+/// which annotations a peer understands; however, in a network of AMQP intermediaries it might
+/// not be possible to know if every intermediary will understand the annotation. Note that for some
+/// annotations it might not be necessary for the intermediary to understand their purpose, i.e.,
+/// they could be used purely as an attribute which can be filtered on.
+///
+/// A registry of defined annotations and their meanings is maintained [AMQPMESSANN].
+///
+/// If the message-annotations section is omitted, it is equivalent to a message-annotations section
+/// containing an empty map of annotations.
 #[derive(Debug, Clone, Default, SerializeComposite, DeserializeComposite)]
 #[amqp_contract(
     name = "amqp:message-annotations:map",
