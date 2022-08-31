a11e87e5125a5ba8ed43ca817504c78879037cfc
minghuaw <michael.wu1107@gmail.com>
2022-08-30 17:56:35 -0700

    impl Eq, Ord, Hash for dyn AnnotationKey

--- fe2o3-amqp-types/src/messaging/format/annotations.rs
+++ fe2o3-amqp-types/src/messaging/format/annotations.rs
@@ -1,12 +1,16 @@
-use std::{borrow::Borrow, collections::BTreeMap};
+use std::{borrow::Borrow, collections::BTreeMap, hash::{Hash, Hasher}};
 
 use serde_amqp::{primitives::{ULong, Symbol, SymbolRef}, Value};
 
 pub type Annotations = BTreeMap<OwnedKey, Value>;
 
+/// Key type for [`Annotations`]
 #[derive(Debug)]
 pub enum OwnedKey {
+    /// Symbol
     Symbol(Symbol),
+
+    /// ULong
     ULong(ULong),
 }
 
@@ -47,9 +51,52 @@ impl AnnotationKey for str {
     }
 }
 
+impl AnnotationKey for String {
+    fn key<'k>(&'k self) -> BorrowedKey<'k> {
+        BorrowedKey::Symbol(SymbolRef(self))
+    }
+}
+
+impl AnnotationKey for Symbol {
+    fn key<'k>(&'k self) -> BorrowedKey<'k> {
+        BorrowedKey::Symbol(SymbolRef(&self.0))
+    }
+}
+
+impl<'a> AnnotationKey for SymbolRef<'a> {
+    fn key<'k>(&'k self) -> BorrowedKey<'k> {
+        BorrowedKey::Symbol(SymbolRef(self.0))
+    }
+}
+
 impl<'a> Borrow<dyn AnnotationKey + 'a> for OwnedKey {
     fn borrow(&self) -> &(dyn AnnotationKey + 'a) {
         self
     }
 }
 
+impl<'a> PartialEq for (dyn AnnotationKey + 'a) {
+    fn eq(&self, other: &Self) -> bool {
+        self.key().eq(&other.key())
+    }
+}
+
+impl<'a> Eq for (dyn AnnotationKey + 'a) { }
+
+impl<'a> PartialOrd for (dyn AnnotationKey + 'a) {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.key().partial_cmp(&other.key())
+    }
+}
+
+impl<'a> Ord for (dyn AnnotationKey + 'a) {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.key().cmp(&other.key())
+    }
+}
+
+impl<'a> Hash for (dyn AnnotationKey + 'a) {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.key().hash(state)
+    }
+}
