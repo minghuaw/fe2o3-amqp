ca577430888355750f3a4e31aad7e8e4800f7552
minghuaw <michael.wu1107@gmail.com>
2022-10-04 13:35:35 -0700

    impl attach and close

--- fe2o3-amqp-cbs/src/client.rs
+++ fe2o3-amqp-cbs/src/client.rs
@@ -1,12 +1,14 @@
 use std::borrow::Cow;
 
+use fe2o3_amqp::{link::DetachError, session::SessionHandle};
 use fe2o3_amqp_management::{
     client::MgmtClient,
-    error::{Error as MgmtError, StatusError},
+    error::{AttachError, Error as MgmtError, StatusError},
     response::Response,
 };
 
 use crate::{
+    constants::{CBS_NODE_ADDR, DEFAULT_CBS_CLIENT_NODE},
     error::CbsClientError,
     put_token::{PutTokenRequest, PutTokenResponse},
 };
@@ -17,12 +19,18 @@ pub struct CbsClient {
 }
 
 impl CbsClient {
-    pub async fn attach() -> Result<Self, CbsClientError> {
-        todo!()
+    pub async fn attach(session: &mut SessionHandle<()>) -> Result<Self, AttachError> {
+        let mgmt_client = MgmtClient::builder()
+            .management_node_address(CBS_NODE_ADDR)
+            .client_node_addr(DEFAULT_CBS_CLIENT_NODE)
+            .attach(session)
+            .await?;
+
+        Ok(Self { mgmt_client })
     }
 
-    pub async fn close(self) {
-        todo!()
+    pub async fn close(self) -> Result<(), DetachError> {
+        self.mgmt_client.close().await
     }
 
     pub async fn put_token(
@@ -31,7 +39,10 @@ impl CbsClient {
         token: impl Into<Cow<'_, str>>,
         entity_type: impl Into<Cow<'_, str>>,
     ) -> Result<(), MgmtError> {
-        let req = PutTokenRequest { name: name.into(), token: token.into() };
+        let req = PutTokenRequest {
+            name: name.into(),
+            token: token.into(),
+        };
         let entity_type = entity_type.into();
         let _accepted = self
             .mgmt_client
--- fe2o3-amqp-cbs/src/constants.rs
+++ fe2o3-amqp-cbs/src/constants.rs
@@ -3,3 +3,5 @@
 pub const CBS_NODE_ADDR: &str = "$cbs";
 
 pub const PUT_TOKEN: &str = "put-token";
+
+pub const DEFAULT_CBS_CLIENT_NODE: &str = "cbs-client-node";
