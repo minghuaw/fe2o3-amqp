8023f290df59f591d814f8695d5de719cd5eca5b
minghuaw <michael.wu1107@gmail.com>
2022-10-08 15:50:17 -0700

    added BodySection trait

--- fe2o3-amqp-types/src/messaging/message/body.rs
+++ fe2o3-amqp-types/src/messaging/message/body.rs
@@ -2,15 +2,46 @@ use std::{fmt::Display, marker::PhantomData};
 
 use serde::{
     de::{self, VariantAccess},
-    Serialize,
+    Serialize, ser,
 };
 use serde_amqp::Value;
 
 use crate::messaging::{AmqpSequence, AmqpValue, Data};
 
+use self::sealed::Sealed;
+
 use super::__private::{Deserializable, Serializable};
 
 use serde_amqp::extensions::TransparentVec;
+
+pub(crate) mod sealed {
+    pub trait Sealed { }
+}
+
+/// Trait for message body
+pub trait BodySection: Sealed + SerializableBodySection + DeserializableBodySection { }
+
+impl<T> BodySection for T where T: Sealed + SerializableBodySection + DeserializableBodySection { }
+
+/// Trait for a serializable body section
+pub trait SerializableBodySection: Sealed {
+    /// The serializable type
+    type Serializable: ser::Serialize;
+
+    /// Get a reference to the serializable type
+    fn serializable(&self) -> &Self::Serializable;
+}
+
+/// Trait for a deserializable body section
+pub trait DeserializableBodySection: Sealed {
+    /// The deserializable type
+    /// 
+    /// TODO: change to GAT once it stablizes
+    type Deserializable: de::DeserializeOwned;
+
+    /// Convert from deserializable to self
+    fn from_deserializable(deserializable: Self::Deserializable) -> Self;
+}
 
 /// The body consists of one of the following three choices: one or more data sections, one or more
 /// amqp-sequence sections, or a single amqp-value section.
