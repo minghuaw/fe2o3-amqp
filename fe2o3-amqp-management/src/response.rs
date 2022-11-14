9f34b7fb4902d9fafe27a8c3273eee1cc55a4221
minghuaw <michael.wu1107@gmail.com>
2022-11-14 06:13:26 -0800

    added docs

--- fe2o3-amqp-management/src/response.rs
+++ fe2o3-amqp-management/src/response.rs
@@ -6,74 +6,11 @@ use crate::{
     status::StatusCode,
 };
 
-// /// The correlation-id of the response message MUST be the correlation-id from the request message
-// /// (if present), else the message-id from the request message. Response messages have the following
-// /// application-properties:
-// pub struct ResponseMessageProperties {
-//     pub correlation_id: MessageId,
-//     pub status_code: StatusCode,
-//     pub status_description: Option<String>,
-// }
-
-// impl ResponseMessageProperties {
-//     pub fn try_take_from_message<T>(message: &mut Message<T>) -> Result<Self, Error> {
-//         let correlation_id = match message
-//             .properties
-//             .as_mut()
-//             .and_then(|p| p.correlation_id.take())
-//         {
-//             Some(correlation_id) => correlation_id,
-//             None => message
-//                 .properties
-//                 .as_mut()
-//                 .and_then(|p| p.message_id.take())
-//                 .ok_or(Error::CorrelationIdAndMessageIdAreNone)?,
-//         };
-
-//         let status_code = match message
-//             .application_properties
-//             .as_mut()
-//             .and_then(|ap| ap.remove(STATUS_CODE))
-//         {
-//             Some(value) => StatusCode::try_from(value).map_err(|_| Error::DecodeError)?,
-//             None => return Err(Error::StatusCodeNotFound),
-//         };
-
-//         let status_description: Option<String> = message
-//             .application_properties
-//             .as_mut()
-//             .and_then(|ap| ap.remove(STATUS_DESCRIPTION))
-//             .map(|value| String::try_from(value).map_err(|_| Error::DecodeError))
-//             .transpose()?;
-
-//         Ok(Self {
-//             correlation_id,
-//             status_code,
-//             status_description,
-//         })
-//     }
-// }
-
-// #[derive(Debug)]
-// pub struct Response<R> {
-//     pub correlation_id: MessageId,
-//     pub status_code: StatusCode,
-//     pub status_description: Option<String>,
-//     pub operation: R,
-// }
-
-// impl<R> Response<R> {
-//     pub fn from_parts(properties: ResponseMessageProperties, operation: R) -> Self {
-//         Self {
-//             correlation_id: properties.correlation_id,
-//             status_code: properties.status_code,
-//             status_description: properties.status_description,
-//             operation,
-//         }
-//     }
-// }
-
-/// Leaving checking particular field of response entirely to the user.
+/// Leaving checking particular field of response entirely to the user. A blanker implementation of
+/// checking the status code is provided in the `check_status_code` method; however, the user will 
+/// need to decide whether to use it or not.
+/// 
+/// This will likely see some significant changes in the future.
 ///
 /// TODO: only allowing one type of body for now as it seems like all responses should know the
 /// exact type of body they are expecting
