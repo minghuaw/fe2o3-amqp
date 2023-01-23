1c3cce9376103a4fd1f0e57cd1a83b3133aa95e5
minghuaw <michael.wu1107@gmail.com>
2023-01-22 22:10:29 -0800

    updated changelog and bumped up version added eg.

---
+++ examples/wasm32-in-browser/index.js
@@ -0,0 +1,9 @@
+import init from "./pkg/wasm32_in_browser.js";
+
+const run = async () => {
+    const wasm = await init("./pkg/wasm32_in_browser_bg.wasm");
+
+    const outcome = wasm.run();
+}
+
+run();
