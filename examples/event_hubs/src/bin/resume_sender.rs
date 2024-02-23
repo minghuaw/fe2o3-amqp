4a383cd3b838bd5d55f978d22a25b9be646c9853
minghuaw <michael.wu1107@gmail.com>
2024-02-22 17:04:50 -0800

    added new example resume sender

--- examples/event_hubs/src/bin/resume_sender.rs
+++ examples/event_hubs/src/bin/resume_sender.rs
@@ -13,6 +13,7 @@ use fe2o3_amqp::{
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+    env_logger::init();
 
     let hostname = env::var("HOST_NAME").unwrap();
     let sa_key_name = env::var("SHARED_ACCESS_KEY_NAME").unwrap();
@@ -39,6 +40,28 @@ async fn main() {
     for i in 0..3 {
         // All of the Microsoft AMQP clients represent the event body as an uninterpreted bag of bytes.
         // A message builder can be used to specify the type of body section
+        let data = format!("Message {}", i).into_bytes();
+        let message = Message::builder()
+            .properties(
+                Properties::builder()
+                    .group_id(String::from("send_to_event_hub"))
+                    .build(),
+            )
+            .data(Binary::from(data))
+            .build();
+        let outcome = sender.send(message).await.unwrap();
+        outcome.accepted_or_else(|outcome| outcome).unwrap();
+    }
+
+    let detached = sender.detach().await.unwrap();
+
+    // Close the old session and create a new one
+    session.close().await.unwrap();
+    let mut session = Session::begin(&mut connection).await.unwrap();
+
+    let mut sender = detached.resume_on_session(&session).await.unwrap();
+
+    for i in 10..13 {
         let data = format!("Message {}", i).into_bytes();
         let message = Message::builder()
             .properties(
