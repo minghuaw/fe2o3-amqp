be8369e115a3380c33cb7ab8783d972fde97a18d
minghuaw <michael.wu1107@gmail.com>
2024-08-06 20:16:31 -0700

    updated examples

--- examples/alternative_tls_connection/src/bin/rustls.rs
+++ examples/alternative_tls_connection/src/bin/rustls.rs
@@ -3,26 +3,22 @@ use std::sync::Arc;
 use fe2o3_amqp::Connection;
 use fe2o3_amqp::Sender;
 use fe2o3_amqp::Session;
+use rustls::pki_types::ServerName;
+use rustls::ClientConfig;
+use rustls::RootCertStore;
 use tokio::net::TcpStream;
 use tokio_rustls::TlsConnector;
 
 #[tokio::main]
 async fn main() {
     let addr = "localhost:5671";
-    let domain = rustls::ServerName::try_from("localhost").unwrap();
+    let domain = ServerName::try_from("localhost").unwrap();
     let stream = TcpStream::connect(addr).await.unwrap();
 
-    let mut root_store = rustls::RootCertStore::empty();
-    root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
-        rustls::OwnedTrustAnchor::from_subject_spki_name_constraints(
-            ta.subject,
-            ta.spki,
-            ta.name_constraints,
-        )
-    }));
-    let config = rustls::ClientConfig::builder()
-        .with_safe_defaults()
-        .with_root_certificates(root_store)
+    let mut root_cert_store = RootCertStore::empty();
+    root_cert_store.extend(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
+    let config = ClientConfig::builder()
+        .with_root_certificates(root_cert_store)
         .with_no_client_auth();
     let connector = TlsConnector::from(Arc::new(config));
     let tls_stream = connector.connect(domain, stream).await.unwrap();
