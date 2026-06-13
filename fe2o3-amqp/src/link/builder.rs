03bb83f5f55871aeb3aba980ea33fca6d4c7caf6
minghuaw <michael.wu1107@gmail.com>
2026-06-13 15:33:01 +0800

    fmt
    Signed-off-by: minghuaw <michael.wu1107@gmail.com>

--- fe2o3-amqp/src/link/builder.rs
+++ fe2o3-amqp/src/link/builder.rs
@@ -1,9 +1,6 @@
 //! Implements the builder for a link
 
-use std::{
-    marker::PhantomData,
-    sync::Arc,
-};
+use std::{marker::PhantomData, sync::Arc};
 
 use fe2o3_amqp_types::{
     definitions::{Fields, ReceiverSettleMode, SenderSettleMode, SequenceNo},
