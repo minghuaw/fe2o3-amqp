3a1989780ce3fe9c92784d394b5e34828a574403
minghuaw <michael.wu1107@gmail.com>
2026-08-11 00:13:35 +0800

    update broker integration test images and add qpid compat tests (#368)

--- fe2o3-amqp/tests/link.rs
+++ fe2o3-amqp/tests/link.rs
@@ -21,6 +21,8 @@ cfg_not_wasm32! {
     async fn test_send_receive_compat() {
         activemq_artemis_send_receive().await;
         activemq_artemis_send_receive_large_content().await;
+        qpid_broker_j_send_receive().await;
+        qpid_broker_j_send_receive_large_content().await;
         rabbitmq_amqp10_send_receive().await;
         rabbitmq_amqp10_send_receive_large_content().await;
     }
@@ -56,6 +58,60 @@ cfg_not_wasm32! {
         let (_node, port) = common::setup_activemq_artemis(None, None).await;
 
         let url = format!("amqp://localhost:{}", port);
+        let mut connection = Connection::open("test-connection", &url[..]).await.unwrap();
+        let mut session = Session::begin(&mut connection).await.unwrap();
+        let mut sender = Sender::attach(&mut session, "test-sender", "test-queue")
+            .await
+            .unwrap();
+        let mut receiver = Receiver::attach(&mut session, "test-receiver", "test-queue")
+            .await
+            .unwrap();
+
+        let message = Message::from("test-message".repeat(100_000));
+        let outcome = sender.send(message).await.unwrap();
+        outcome.accepted_or("Not accepted").unwrap();
+
+        let received = receiver.recv::<String>().await.unwrap();
+        receiver.accept(&received).await.unwrap();
+        assert_eq!(received.body(), &"test-message".repeat(100_000));
+
+        sender.close().await.unwrap();
+        receiver.close().await.unwrap();
+        session.close().await.unwrap();
+        connection.close().await.unwrap();
+    }
+
+    async fn qpid_broker_j_send_receive() {
+        let (_node, port) = common::setup_qpid_broker_j(None, None).await;
+
+        let url = format!("amqp://admin:admin@localhost:{}", port);
+        let mut connection = Connection::open("test-connection", &url[..]).await.unwrap();
+        let mut session = Session::begin(&mut connection).await.unwrap();
+        let mut sender = Sender::attach(&mut session, "test-sender", "test-queue")
+            .await
+            .unwrap();
+        let mut receiver = Receiver::attach(&mut session, "test-receiver", "test-queue")
+            .await
+            .unwrap();
+
+        let message = Message::from("test-message");
+        let outcome = sender.send(message).await.unwrap();
+        outcome.accepted_or("Not accepted").unwrap();
+
+        let received = receiver.recv::<String>().await.unwrap();
+        receiver.accept(&received).await.unwrap();
+        assert_eq!(received.body(), "test-message");
+
+        sender.close().await.unwrap();
+        receiver.close().await.unwrap();
+        session.close().await.unwrap();
+        connection.close().await.unwrap();
+    }
+
+    async fn qpid_broker_j_send_receive_large_content() {
+        let (_node, port) = common::setup_qpid_broker_j(None, None).await;
+
+        let url = format!("amqp://admin:admin@localhost:{}", port);
         let mut connection = Connection::open("test-connection", &url[..]).await.unwrap();
         let mut session = Session::begin(&mut connection).await.unwrap();
         let mut sender = Sender::attach(&mut session, "test-sender", "test-queue")
